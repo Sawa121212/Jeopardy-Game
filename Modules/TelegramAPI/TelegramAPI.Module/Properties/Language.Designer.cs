1260690d1f33305f962a5e16914ddb34332dd1ea
Aleksandr Nikolaev <sanek22cs@gmail.com>
2024-07-09 11:47:45 +0300

    Version 0.0.0.1 Update .NET up to version 7.0

--- Modules/TelegramAPI/TelegramAPI.Module/Properties/Language.Designer.cs
+++ Modules/TelegramAPI/TelegramAPI.Module/Properties/Language.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace TelegramAPI.Module.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -16,7 +19,7 @@ namespace TelegramAPI.Module.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Language {
@@ -36,7 +39,7 @@ namespace TelegramAPI.Module.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TelegramAPI.Test.Properties.Language", typeof(Language).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TelegramAPI.Module.Properties.Language", typeof(Language).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
